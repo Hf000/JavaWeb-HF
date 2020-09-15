7be58a0f7a155d3d60c6903e95cdaef931431f3d
胡飞 <809969840@qq.com>
2020-09-15 16:41:58 +0800

    hf update

---
+++ src/com/hufei/algorithm/test.java
@@ -0,0 +1,17 @@
+package com.hufei.algorithm;
+
+/**
+ * @Author:hufei
+ * @CreateTime:2020-09-15
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        //System.out.println(System.currentTimeMillis());
+        Snowflake snow = new Snowflake(1,1);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(snow.getId());
+        }
+    }
+
+}
